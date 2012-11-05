fcac2209ffbd879163e3d2d9a7a7bb4c9eba707f
Dane Gardner <dane.gardner@gmail.com>
2012-11-04 21:20:37 -0800

    Added assert_failed() to main.c; cleaned up stm32f4 configuration header

--- src/main.c
+++ src/main.c
@@ -79,3 +79,12 @@ int main(void)
     xTaskCreate(init_task, (signed char*)"init", 1024, NULL, tskIDLE_PRIORITY, NULL);
     vTaskStartScheduler();
 }
+
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  printf("assert_failed(%s, %d);\n", file, line) ;
+  while(1);
+}
+#endif
